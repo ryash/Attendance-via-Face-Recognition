aba5f318810d20fd9c47fcfecae8c074bd2b8163
krak-ken <aniketsahu899@gmail.com>
2019-10-31 12:06:55 +0530

    Master Cleaned

--- Server/handlers/error.js
+++
@@ -1,9 +0,0 @@
-function errorHandler(err, req, res, next){
-	return res.status(err.status || 500).json({
-		error: {
-			message: err.message || 'Oops! Something went wrong.'
-		}
-	});
-}
-
-module.exports = errorHandler;
