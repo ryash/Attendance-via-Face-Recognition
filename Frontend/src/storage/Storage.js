b7e000f0039f575d80e4dbbc53fffef308be1aeb
ryash <yashkhanna1974@gmail.com>
2019-11-15 15:17:30 +0530

    fix branches-1

--- Frontend/src/storage/Storage.js
+++
@@ -1,6 +0,0 @@
-import {AsyncStorage} from 'react-native';
-
-// Exports the Storage Object used to store all the data.
-// The implementation of this storage object methods can be modified
-// as long as the api provided is the same.
-export default AsyncStorage;
